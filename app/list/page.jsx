f65380efcde9a1bf37fc0e68bce06ef0a5000256
Jonghyeok <jhpmuscle@gmail.com>
2024-07-01 23:52:09 +0900

    NEXT 반복 UI 처리

--- app/list/page.jsx
+++ app/list/page.jsx
@@ -1,13 +1,17 @@
 export default function List() {
+  let products = ['Tomatoes', 'Pasta', 'Coconut'];
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품 1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품 2 $40</h4>
-      </div>
+      {
+        products.map((item, i) => {
+          return (
+            <div className="food" key={i}>
+              <h4>{item} $40</h4>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
