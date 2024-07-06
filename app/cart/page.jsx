6896449ca61a28787648738feb066a3dd659a3e0
Jonghyeok <jhpmuscle@gmail.com>
2024-07-07 00:31:00 +0900

    컴포넌트 반복

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,18 +1,23 @@
 export default function Cart() {
+  let cartItems = ['Tomatoes', 'Pasta']
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      {
+        cartItems.map((item, i) => {
+          return <CartItem name={item} key={i} />
+        })
+      }
     </div>
   )
 } 
 
-function CartItem() {
+function CartItem(props) {
+  // Next에서는 컴포넌트 내에서 fetch로 가져오는 경우도 꽤 있음
+  // hook을 사용하게 되면 use client 해야하므로 SSR이 불가능하게 됨
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.name}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
