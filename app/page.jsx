82f5e95bd5f0cc5af02749e5267fd9770b97065d
Jonghyeok <jhpmuscle@gmail.com>
2024-07-13 03:07:12 +0900

    CSR, DB연동

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-export default function Home() {
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
   const name = "Park";
+  let client = await connectDB;
+  const db = client.db('forum');
+  let result = await db.collection('post').find().toArray();
   return (
     <div>
       <h4 className="title">애플후레시</h4>
       <p className="title-sub">by dev {name}</p>
+      <p>{result[0].title}</p>
     </div>
   );
 }
